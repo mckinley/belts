6d802521e4a8c60a6cd63545287803cfef11cfe7
Bronson McKinley <bronsonmckinley@gmail.com>
2019-08-25 21:45:06 -0700

    clean up

--- template.rb
+++ template.rb
@@ -12,7 +12,6 @@ def add_gems
     gem 'awesome_print'
     gem 'rspec-rails'
     gem 'factory_bot_rails'
-    gem 'poltergeist'
     gem 'pry-rails'
     gem 'pry-byebug'
     gem 'bundler-audit', require: false
@@ -28,7 +27,7 @@ def add_gems
 
   after_bundle do
     generate 'rspec:install'
-    copy_file 'spec_support_capybara.rb', 'spec/support/capybara.rb'
+    copy_file 'spec/support/system.rb', 'spec/support/system.rb'
     uncomment_lines 'spec/rails_helper.rb', 'each { |f| require f }'
   end
 end
@@ -68,6 +67,7 @@ def create_heroku_apps
   create_setup_script
   set_heroku_application_host
   set_heroku_rails_secrets
+  set_heroku_backup_schedule
 end
 
 def create_base_dotfiles
@@ -76,7 +76,7 @@ def create_base_dotfiles
 end
 
 def create_deploy_script
-  copy_file 'bin_deploy', 'bin/deploy'
+  copy_file 'bin/deploy', 'bin/deploy'
 
   instructions = <<~MARKDOWN
 
@@ -93,7 +93,7 @@ def create_deploy_script
 end
 
 def create_review_apps_setup_script
-  template 'bin_setup-review-app.erb', 'bin/setup-review-app'
+  template 'bin/setup-review-app.erb', 'bin/setup-review-app'
   run 'chmod a+x bin/setup-review-app'
 end
 
@@ -107,28 +107,22 @@ end
 
 def create_staging_heroku_app(flags)
   app_name = heroku_app_name_for('staging')
-  run_toolbelt_command "create #{app_name} #{flags}", 'staging'
+  run_heroku_command "create #{app_name} #{flags}", 'staging'
 end
 
 def create_production_heroku_app(flags)
   app_name = heroku_app_name_for('production')
-  run_toolbelt_command "create #{app_name} #{flags}", 'production'
+  run_heroku_command "create #{app_name} #{flags}", 'production'
 end
 
 def create_setup_script
-  pipelines_plugin = `heroku help | grep pipelines`
-  if pipelines_plugin.empty?
-    puts 'You need heroku pipelines plugin. Run: brew upgrade heroku-toolbelt'
-    exit 1
-  end
-
-  template 'bin_setup.erb', 'bin/setup'
+  template 'bin/setup.erb', 'bin/setup', force: true
   run 'chmod a+x bin/setup'
 end
 
 def set_heroku_application_host
   %w(staging production).each do |environment|
-    run_toolbelt_command(
+    run_heroku_command(
       "config:add APPLICATION_HOST=#{heroku_app_name}-#{environment}.herokuapp.com",
       environment
     )
@@ -137,24 +131,20 @@ end
 
 def set_heroku_rails_secrets
   %w(staging production).each do |environment|
-    run_toolbelt_command(
+    run_heroku_command(
       "config:add SECRET_KEY_BASE=#{generate_secret}",
       environment
     )
   end
 end
 
-def command_to_join_heroku_app(environment)
-  heroku_app_name = heroku_app_name_for(environment)
-  <<~SHELL
-
-    if heroku join --app #{heroku_app_name} > /dev/null 2>&1; then
-      git remote add #{environment} git@heroku.com:#{heroku_app_name}.git || true
-      printf 'You are a collaborator on the "#{heroku_app_name}" Heroku app\n'
-    else
-      printf 'Ask for access to the "#{heroku_app_name}" Heroku app\n'
-    fi
-  SHELL
+def set_heroku_backup_schedule
+  %w(staging production).each do |environment|
+    run_heroku_command(
+      "pg:backups:schedule DATABASE_URL --at '10:00 UTC'",
+      environment
+    )
+  end
 end
 
 def heroku_app_name
@@ -169,12 +159,8 @@ def generate_secret
   SecureRandom.hex(64)
 end
 
-def run_toolbelt_command(command, environment)
+def run_heroku_command(command, environment)
   run("heroku #{command} --remote #{environment}")
-end
-
-def source_paths
-  [(Pathname.new(__FILE__).dirname + 'lib').expand_path]
 end
 
 create_base_setup
